ad79d993b1811d0d5904eadf0bced55fbe751b57
merizrizal <meriz.rizal@gmail.com>
2025-01-29 13:50:22 +0700

    Fix composer download script

--- base/config/install-script.sh
+++ base/config/install-script.sh
@@ -17,7 +17,6 @@ cp /root/initial-config/99-config_overrides.ini /etc/php/7.4/apache2/conf.d/99-c
 a2enmod rewrite
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 php -r "unlink('composer-setup.php');"
 
